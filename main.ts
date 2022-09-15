3de024c6dbe236360525c070b63d69c527bd01f0
SeeeJay <113055585+SeeeJay@users.noreply.github.com>
2022-09-15 16:49:46 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,12 +2,6 @@ input.onButtonPressed(Button.A, function () {
     num += 1
     basic.showNumber(num)
 })
-input.onGesture(Gesture.TiltLeft, function () {
-    sprite.change(LedSpriteProperty.X, -1)
-})
-input.onButtonPressed(Button.AB, function () {
-	
-})
 input.onButtonPressed(Button.B, function () {
     num += -1
     basic.showNumber(num)
@@ -16,14 +10,10 @@ input.onGesture(Gesture.Shake, function () {
     num = 0
     basic.showNumber(num)
 })
-input.onGesture(Gesture.TiltRight, function () {
-    sprite.change(LedSpriteProperty.X, 1)
-})
-let sprite: game.LedSprite = null
 let num = 0
 basic.showIcon(IconNames.Heart)
 num = 1
-sprite = game.createSprite(2, 2)
+basic.showNumber(num)
 basic.forever(function () {
 	
 })
